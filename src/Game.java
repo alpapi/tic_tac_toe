910863f918ebf8aa8cec667b9cebfb70f3a6e4ca
alpapi <alexrapp025@yahoo.com>
2017-03-16 20:28:01 -0400

    Removed the hi variable

--- src/Game.java
+++ src/Game.java
@@ -8,7 +8,6 @@ public class Game {
 	boolean player1 = true, // Player 1 is X's, player 2 is O's
 	player2 = false,
 	winner = false;
-    String hi = "";
 	
 	String[][] positions = new String[3][3];
 	String[] posNames = {"r1c1", "r1c2", "r1c3", "r2c1", "r2c2", "r2c3", "r3c1", "r3c2", "r3c3"};
